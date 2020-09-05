e38a2a776a3996b831e271fc6c967fbfa99442d1
Pedro Xudre <pedro@xudre.com>
2020-09-05 09:59:29 -0300

    Custom app for Koin injection

---
+++ app/src/main/java/br/com/psyapp/PsyApp.kt
@@ -0,0 +1,23 @@
+package br.com.psyapp
+
+import android.app.Application
+import br.com.psyapp.lib.emotions.Emotions
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class PsyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@PsyApp)
+            modules(listOf())
+        }
+
+        Emotions.getInstance().init()
+    }
+
+}
