a9ed89b58b9d3e46237a87367739d5f82cc0b600
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2020-09-12 13:06:19 -0300

    feat: add phone call

--- app/src/main/java/br/com/psyapp/ui/map/MapFragment.kt
+++ app/src/main/java/br/com/psyapp/ui/map/MapFragment.kt
@@ -1,5 +1,7 @@
 package br.com.psyapp.ui.map
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +14,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.fragment_map.*
 
@@ -20,15 +23,17 @@ import kotlinx.android.synthetic.main.fragment_map.*
  * Use the [MapFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MapFragment : BaseFragment(), OnMapReadyCallback {
+class MapFragment : BaseFragment(), OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener  {
 
     private val mapViewModel by viewModels<MapViewModel>()
     private lateinit var mMap: GoogleMap
 
+    override fun onInfoWindowClick(p0: Marker?) {
+        makeCall(p0?.snippet.toString())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val mapFragment = activity?.supportFragmentManager?.findFragmentById(R.id.mv) as? SupportMapFragment
-//        mapFragment?.getMapAsync(this)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -73,15 +78,34 @@ class MapFragment : BaseFragment(), OnMapReadyCallback {
     override fun onMapReady(p0: GoogleMap?) {
         p0?.let { googleMap ->
             mMap = googleMap
+            mMap.setOnInfoWindowClickListener(this)
+
             mMap.setMinZoomPreference(1f)
             val worker1 = LatLng(-23.564257, -46.652689)
-            mMap.addMarker(MarkerOptions().position(worker1).title("João da Silva - Psicólogo"))
+            mMap.addMarker(
+                MarkerOptions().position(worker1).title("João da Silva - Psicólogo").snippet(
+                    "+5511965656534"
+                )
+            )
             val worker2 = LatLng(-23.569197, -46.651114)
-            mMap.addMarker(MarkerOptions().position(worker2).title("Everton Ribeiro - Psiquiatra"))
+            mMap.addMarker(
+                MarkerOptions().position(worker2).title("Everton Ribeiro - Psiquiatra").snippet(
+                    "+5511965656534"
+                )
+            )
             val worker3 = LatLng(-23.558836, -46.656790)
-            mMap.addMarker(MarkerOptions().position(worker3).title("Marcos Paulo - Coach"))
+            mMap.addMarker(
+                MarkerOptions().position(worker3).title("Marcos Paulo - Coach")
+                    .snippet("+5511965656534")
+            )
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(worker1, 15f))
         }
+    }
+
+    fun makeCall(phone: String) {
+        val intent = Intent(Intent.ACTION_DIAL);
+        intent.data = Uri.parse("tel:+$phone");
+        startActivity(intent);
     }
 
     companion object {
